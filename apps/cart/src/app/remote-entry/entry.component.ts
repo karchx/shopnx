f71761c8a2d015c947fe4ac64fb06999f4954abd
karchx <kenethra.74@gmail.com>
2023-03-24 15:59:32 -0600

    init project

---
+++ apps/cart/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shopnx-cart-entry',
+  template: `<shopnx-nx-welcome></shopnx-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
---
+++ apps/shop/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shopnx-shop-entry',
+  template: `<shopnx-nx-welcome></shopnx-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
