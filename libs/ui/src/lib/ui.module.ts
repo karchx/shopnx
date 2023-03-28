17712c44ce7bb947bd95fae0d74ccdfe7b8a1cc5
karchx <kenethra.74@gmail.com>
2023-03-28 09:18:23 -0600

    style: add primeng

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommonComponent } from './layout/common/common.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    
+    ButtonModule
+  ],
   declarations: [CommonComponent],
   exports: [CommonComponent]
 })
